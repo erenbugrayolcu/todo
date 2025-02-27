de9aa03b42fb431c13dfce6a402d3a4db04347c3
Eren Bugra YOLCU <yolcubugra7@gmail.com>
2025-02-27 12:31:28 +0300

    First commit

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color textColor = Colors.white;
+  static const Color textColor = Colors.white60;
   static const Color buttonColor = Colors.blue;
   static const Color backColor = Color.fromARGB(220, 33, 33, 33);
   static const Color appBarColor = Color.fromARGB(220, 33, 33, 33);
